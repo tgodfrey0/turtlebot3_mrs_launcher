7ce1835df41e0074fd0d33a53620ac7f85e41a64
Toby Godfrey <toby.godfrey2003@gmail.com>
2024-08-21 13:48:42 +0100

    keep old param

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 NAMESPACE="turtlebot_$(ip -brief link show | grep -v LOOPBACK | grep -v "DOWN" | head -n 1 | sed 's/ /\n/g' | grep -v "^$" | head -n 3 | tail -n 1 | cut -d ':' -f4- | tr ":" "_")"
 echo "export MRS_ROS_NAMESPACE='$NAMESPACE'" >> ~/.bashrc
 echo "Please source ~/.bashrc"
+
+cp param/$TURTLEBOT3_MODEL.yaml param/$TURTLEBOT3_MODEL.old.yaml
 sed -i 's/<<MRS_ROS_NAMESPACE>>/'"$NAMESPACE"'/g' param/$TURTLEBOT3_MODEL.yaml
